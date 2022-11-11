ed86e390ba4fdd0a59d0ac694ba938290ee9d66e
Prateek Gupta <prateekgupta.nitjsr@gmail.com>
2022-11-11 18:54:23 +0100

    Time: 139 ms (26.22%), Space: 14 MB (75.06%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -9,7 +9,6 @@ class Solution:
         T[0]=0
         T[1]=0
         T[2]=min(cost[0],cost[1])
-        # print(T)
         for i in range(3,len(T)):
             T[i]=min(T[i-1]+cost[i-1],T[i-2]+cost[i-2])
             

6057cadecfcc0cb8d3d7194c354e4e6898c8ba03
Prateek Gupta <prateekgupta.nitjsr@gmail.com>
2022-12-25 12:33:26 +0100

    Time: 58 ms (99.56%), Space: 14.5 MB (53.11%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -6,17 +6,15 @@ class Solution:
         visited=collections.defaultdict(bool)
         for i in range(num_rooms):
             visited[i]=False
-        
+        visited[0]=True
         stack=[rooms[0]]
         while stack:
             room=stack.pop()
-            print(room)
-
             for i in room:                
                 if visited[i]==False:
                     visited[i]=True
                     stack.append(rooms[i])
-        visited[0]=True
+        
         for i in visited:
             if visited[i]==False:
                 return False

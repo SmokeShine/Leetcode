61bc60b55d9985546f78197bf6148e5fda0496b5
Prateek Gupta <prateekgupta.nitjsr@gmail.com>
2023-01-09 14:15:12 +0100

    Time: 39 ms (33.21%), Space: 13.8 MB (98.59%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -2,33 +2,20 @@ class MyQueue:
 
     def __init__(self):
         self.stack1=[]
-        self.stack2=[]
-
-    def push(self, x: int) -> None:
-        if len(self.stack1)<10:
-            #use stack2
-            self.stack1.append(x)
-        else:
-            self.stack2.append(x)
-
-    def pop(self) -> int:
-        if not self.stack1:
-            #use stack2
-            temp=self.stack2[0]
-            self.stack2=[self.stack2[i] for i in range(1,len(self.stack2))]
         
-        else:
-            temp=self.stack1[0]
-            self.stack1=[self.stack1[i] for i in range(1,len(self.stack1))]
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
+        
+    def pop(self) -> int:
+        temp=self.stack1[0]
+        self.stack1=[self.stack1[i] for i in range(1,len(self.stack1))]
         return temp
 
     def peek(self) -> int:
-        if self.stack1:
-            return self.stack1[0]
-        return self.stack2[0]
-
+        return self.stack1[0]
+        
     def empty(self) -> bool:
-        if not self.stack1 and not self.stack2:
+        if not self.stack1:
             return True
         return False
 
